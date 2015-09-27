0593d3cd07dade7559ed0ef666c71886c0089daf
Gaurav Tiwari <er.grvtiwari@gmail.com>
2015-09-28 02:01:13 +0530

    Added Mailer

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -1,11 +1,48 @@
 var memberList 		= require('../models/memberModel');
 
 var http 	 		= require('http');
+var Mailgun = require('mailgun-js');
+
+var sendMail = function (to, subject, content) {
+    var mailGun = new Mailgun({
+        apiKey: 'Will-Be-Mailed',
+        domain: 'IndiaRoasts.com'
+    });
+
+    var mailObj = {
+        from : 'Roaster <Roast@indiaroasts.com>',
+        to : to,
+        subject : subject,
+        html: content
+    };
+
+    mailGun.messages().send(mailObj, function (err, body) {
+        console.log( err );
+        console.log( body );
+    });
+};
+
+var sendRegistrationMail = function( userInfo ) {
+	console.log('Sending Registration Mail' );
+	var template = '<p>Hey {{userName}} !</p>' + 
+					'<p>' + 'You are successfully registered to roast the hell out. Comment, debate, vote and roast about whatever you want to on the slow heat of sarcasms.' + '</p>' +
+					'<p>' + 'We are providing you with the first platform of its kind. You get to speak out the confined thoughts you\'ve always had and get to know what others think about it. ' + '</p>' + 
+					'<p>' + 'So set the trend! As we give you this new adda to speak on any thing, anyway, about anyone.' + '</p>' +
+					'<p>' + 'Don\'t keep in anymore..take it out!! Roast it..' + '</p>' + 					
+					'<p>' + 'PS: You can post comments anonymously without even logging in.' + '</p>' +					
+					'<p>' + 'Respect,' + 
+					'<p>' + 'Team Roasters' + '</p>';
+	var htmlContent = template.replace(new RegExp('{{' + 'userName' + '}}', 'i'), userInfo.name );
+	var subject = 'Ready to roast!!';
+
+	sendMail( userInfo.email, subject, htmlContent );
+};
 
 
 module.exports.createMember = function(req, res){
 
 	var query = req.user;
+	console.log( 'In create Member' );
 
 	if ( typeof query !== 'undefined' && query ) {
 
@@ -26,8 +63,9 @@ module.exports.createMember = function(req, res){
 
 				var newMember = new member(memberInfo);
 					        
-				newMember.save(function(err, result){
-						res.json('success');
+				newMember.save(function(err, result){						
+						res.json('success');								
+						sendRegistrationMail( memberInfo );				
 				});
 			}else if (typeof facebookUser !== 'undefined' && facebookUser) {
 				var memberInfo = {
@@ -40,8 +78,9 @@ module.exports.createMember = function(req, res){
 
 				var newMember = new member(memberInfo);
 					        
-				newMember.save(function(err, result){
-						res.json('success');
+				newMember.save(function(err, result){						
+						res.json('success');		
+						sendRegistrationMail( memberInfo );				
 				});
 			};
 
