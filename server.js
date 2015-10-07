31061d93ee63c8310d3f1e6b9de7486dff9c85a1
Gaurav Tiwari <er.grvtiwari@gmail.com>
2015-10-07 23:39:44 +0530

    Added EJS support

--- server.js
+++ server.js
@@ -27,12 +27,24 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 require('./server/config/passport')(passport);
 
-app.engine('html', require('ejs').renderFile); //TODO npm install ejs
-app.set('view engine', 'html');
-app.use('/', express.static(__dirname + '/client'));
+/*app.engine('html', require('ejs').renderFile); //TODO npm install ejs
+app.set('view engine', 'html');*/
+app.set('view engine', 'ejs');  
+app.get('/appResources/*', function( req, res, next){
+	res.sendFile(__dirname + '/client/' + req.originalUrl );	
+});
+
+app.get('/images/*', function( req, res, next){
+	res.sendFile(__dirname + '/client/' + req.originalUrl );	
+});
+
+app.get('/views/*', function( req, res, next){
+	res.sendFile(__dirname + '/client/' + req.originalUrl );	
+});
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/client/index.html');
+	console.log( 'Sending EJS file' );
+	res.render(__dirname + '/client/index.ejs', {title : 'You are dumb.'});
 });
 
 app.get('/roast/:id', function(req, res){
